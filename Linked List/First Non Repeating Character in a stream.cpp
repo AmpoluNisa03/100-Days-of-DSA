7f1d6f73795070353f5727c4c41fa116555ba975
Ampolu Nisa <63179438+AmpoluNisa03@users.noreply.github.com>
2021-07-26 21:40:06 +0530

    Update First Non Repeating Character in a stream.cpp

--- Linked List/First Non Repeating Character in a stream.cpp
+++ Linked List/First Non Repeating Character in a stream.cpp
@@ -3,32 +3,35 @@ using namespace std;
 
 class Solution
 {
-public:
-string FirstNonRepeating(string A)
-{
-string s = "";
-queue<char> q;
-vector<int> v(26, 0);
-for (auto i : A) {
-q.push(i);
-v[i - 'a']++;
-while (!q.empty() and v[q.front() - 'a']>1) q.pop();
-s += q.empty()?'#':q.front();
-}
-return s;
-}
+  public:
+  string FirstNonRepeating(string A)
+  {
+    string s = "";
+    queue<char> q;
+    vector<int> v(26, 0);
+    for (auto i : A) 
+    {
+      q.push(i);
+      v[i - 'a']++;
+      while (!q.empty() and v[q.front() - 'a']>1) q.pop();
+      s += q.empty()?'#':q.front();
+    }
+    return s;
+  }
 };
+
 int main()
 {
-int tc;
-cin>>tc;
-while(tc--)
-{
-string A;
-cin>>A;
-Solution obj;
-string ans=obj.FirstNonRepeating(A);
-cout<<ans<<"\n";
-}
-return 0;
+  int tc;
+  cin>>tc;
+  while(tc--)
+  {
+    string A;
+    cin>>A;
+    Solution obj;
+    string ans=obj.FirstNonRepeating(A);
+    cout<<ans<<"\n";
+  }
+  return 0;
+  
 }
